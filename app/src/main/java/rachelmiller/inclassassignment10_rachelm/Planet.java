b923bcfe0758fc99ce2c4fc7651e9ab8f857a7e9
rachelmiller <rkm2314@tc.columbia.edu>
2017-04-11 09:45:27 -0400

    first commit

---
+++ app/src/main/java/rachelmiller/inclassassignment10_rachelm/Planet.java
@@ -0,0 +1,18 @@
+package rachelmiller.inclassassignment10_rachelm;
+
+/**
+ * Created by rachelmiller on 4/10/17.
+ */
+
+public class Planet {
+    public String name;
+    public String info;
+    public int photoId;
+
+    public Planet(String name, String info, int photoId) {
+        this.name = name;
+        this.info = info;
+        this.photoId = photoId;
+    }
+
+}
